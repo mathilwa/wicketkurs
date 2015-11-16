a80a71615d0cb021451d52f9a2772ce7b583915a
Wærstad, Mathilde <Mathilde.Werstad@nav.no>
2015-11-16 15:04:48 +0100

    Skriver ny model på en annen måte

--- src/main/java/no/nav/wicketkurs/web/panels/LeggTilKommentarPanel.java
+++ src/main/java/no/nav/wicketkurs/web/panels/LeggTilKommentarPanel.java
@@ -12,25 +12,29 @@ import org.apache.wicket.model.Model;
 
 public class LeggTilKommentarPanel extends Panel {
 
-    private DBService dbService;
+    private DBService dbService = DBServiceImpl.getInstance();
 
     public LeggTilKommentarPanel(String id) {
         super(id);
 
-        dbService = DBServiceImpl.getInstance();
-
-        final TextField navnFelt = new TextField("navn", Model.of(""));
+        final TextField navnFelt = new TextField("navn", new Model());
+        final TextField kommentartekstFelt = new TextField("kommentar", new Model());
+        final TextField mailFelt = new TextField("mail", new Model());
 
         Form kommentarForm = new Form("kommentarForm") {
             @Override
             protected void onSubmit() {
                 System.out.println("Lagt til");
-                dbService.leggTilKommentar(new Kommentar(navnFelt.getModelObject().toString(), "", ""));
+                dbService.leggTilKommentar(new Kommentar(navnFelt.getModelObject().toString(), mailFelt.getModelObject().toString(), kommentartekstFelt.getModelObject().toString()));
             }
         };
 
         kommentarForm.add(new Label("navnLabel", "Navn"));
+        kommentarForm.add(new Label("kommentarLabel", "Kommentar"));
+        kommentarForm.add(new Label("mailLabel", "Mail"));
         kommentarForm.add(navnFelt);
+        kommentarForm.add(kommentartekstFelt);
+        kommentarForm.add(mailFelt);
 
         add(kommentarForm);
     }
